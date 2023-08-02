9b785366a6bc0106d90d4e4b693ce772fb3e1b9d
irrationnelle <drakkarverenis@gmail.com>
2023-08-02 18:38:32 +0900

    draft: writing axios

--- packages/plantae/src/createMiddleware.spec.ts
+++ packages/plantae/src/createMiddleware.spec.ts
@@ -1,3 +1,4 @@
+import type { AxiosInstance, AxiosResponseHeaders } from "axios";
 import type ky from "ky";
 import { describe, expect, test } from "vitest";
 
@@ -56,47 +57,48 @@ describe("createMiddleware -", () => {
     expect(await response.text()).toBe("hello plantae");
   });
 
-  test("ky", async () => {
+  test("axios", async () => {
     const initialRequest = new Request("https://example.com", {});
     const initialResponse = new Response("hello world", {});
 
-    const { requestMiddleware, responseMiddleware } = createMiddleware<
-      typeof ky
-    >({
-      convertToAdapterRequest: (req) => {
-        return {
-          body: req.body,
-          headers: req.headers,
-          method: req.method,
-          url: req.url,
-          signal: req.signal,
-        };
-      },
-      convertToAdapterResponse: (res) => ({
-        body: res.body,
-        headers: res.headers,
-        url: res.url,
-        ok: res.ok,
-        status: res.status,
-        statusText: res.statusText,
-      }),
-      extendClientRequest: () => {
-        return new Request("https://example2.com", {
-          headers: new Headers({
-            foo: "bar",
-          }),
-        });
-      },
-      extendClientResponse: () => {
-        return new Response("hello plantae", {
-          headers: new Headers({
-            baz: "qux",
-          }),
-        });
-      },
-      plugins: [],
-      retry: () => Promise.resolve(new Response()),
-    });
+    const { requestMiddleware, responseMiddleware } =
+      createMiddleware<AxiosInstance>({
+        convertToAdapterRequest: (req) => {
+          return {
+            body: req.data,
+            headers: new Headers(req.headers.toJSON(true) as HeadersInit),
+            method: req.method!,
+            url: req.url!,
+            signal: req.signal as AbortSignal,
+          };
+        },
+        convertToAdapterResponse: (res) => ({
+          body: res.data,
+          headers: new Headers(
+            (res.headers as AxiosResponseHeaders).toJSON(true) as HeadersInit
+          ),
+          url: res.url,
+          ok: res.ok,
+          status: res.status,
+          statusText: res.statusText,
+        }),
+        extendClientRequest: () => {
+          return new Request("https://example2.com", {
+            headers: new Headers({
+              foo: "bar",
+            }),
+          });
+        },
+        extendClientResponse: () => {
+          return new Response("hello plantae", {
+            headers: new Headers({
+              baz: "qux",
+            }),
+          });
+        },
+        plugins: [],
+        retry: () => Promise.resolve(new Response()),
+      });
 
     const request = await requestMiddleware(initialRequest);
     const response = await responseMiddleware(initialResponse, request);
