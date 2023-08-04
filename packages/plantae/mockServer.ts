f7cd11c0ef00ff5a6d37845518e96cc056b3ea34
irrationnelle <drakkarverenis@gmail.com>
2023-08-04 11:53:23 +0900

    test: Add test for axios adapter

--- packages/plantae/mockServer.ts
+++ packages/plantae/mockServer.ts
@@ -3,6 +3,10 @@ import { setupServer } from "msw/node";
 
 const handlers = [
   rest.get("https://example.com/api/v1/foo", (req, res, ctx) => {
+    if (req.headers.get("x-request-plugin") === "activate") {
+      return res(ctx.text("request plugin is activated"));
+    }
+
     const mockJson = {
       foo: "bar",
     };
--- packages/plantae/src/axios/createAxiosInterceptors.spec.ts
+++ packages/plantae/src/axios/createAxiosInterceptors.spec.ts
@@ -2,20 +2,203 @@ import type { AxiosHeaders } from "axios";
 import axios from "axios";
 import { describe, expect, test } from "vitest";
 
-import type { AdapterResponse } from "../types";
+import type { Plugin } from "../types";
 import createAxiosInterceptors from "./createAxiosInterceptors";
 
-describe("axios -", () => {
+describe("axios:beforeRequest -", () => {
   test("headers", async () => {
     const axiosInstance = axios.create({
       baseURL: "https://example.com",
     });
 
-    const myPlugin = () => ({
+    const myPlugin = (): Plugin => ({
       name: "myPlugin",
       hooks: {
-        afterResponse: async (res: AdapterResponse) => {
+        beforeRequest: async (req) => {
+          req.headers.set("x-request-plugin", "activate");
+          return req;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [myPlugin()],
+    });
+
+    axiosInstance.interceptors.request.use(axiosMiddleware.request);
+
+    const res = await axiosInstance.get("/api/v1/foo");
+    expect(res.data).toStrictEqual("request plugin is activated");
+  });
+});
+
+describe("axios:afterResponse -", () => {
+  test("headers", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const myPlugin = (): Plugin => ({
+      name: "myPlugin",
+      hooks: {
+        afterResponse: async (res) => {
           res.headers.set("x-foo", "bar");
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [myPlugin()],
+    });
+
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+    const customHeader = (res.headers as AxiosHeaders)["x-foo"];
+
+    if (!customHeader) {
+      throw new Error("custom header not found");
+    }
+
+    expect(customHeader).toStrictEqual("bar");
+  });
+
+  test("body", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const myPlugin = (): Plugin => ({
+      name: "myPlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          res.body = "baz";
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [myPlugin()],
+    });
+
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.data).toBe("baz");
+  });
+
+  test("overriden plugin", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const myPlugin = (): Plugin => ({
+      name: "myPlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          res.body = "first";
+          res.headers.set("x-first", "foo");
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [myPlugin()],
+    });
+
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const overridenPlugin = (): Plugin => ({
+      name: "overridenPlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          res.body = "second";
+          res.headers.set("x-second", "bar");
+          return res;
+        },
+      },
+    });
+
+    const overridenAxiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [overridenPlugin()],
+    });
+
+    axiosInstance.interceptors.response.use(overridenAxiosMiddleware.response);
+
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.headers["x-first"]).toBe("foo");
+    expect(res.headers["x-second"]).toBe("bar");
+    expect(res.data).toBe("second");
+  });
+
+  test("multiple plugins", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const myPlugin = (): Plugin => ({
+      name: "myPlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          res.body = "first";
+          res.headers.set("x-first", "foo");
+          return res;
+        },
+      },
+    });
+
+    const secondPlugin = (): Plugin => ({
+      name: "secondPlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          res.body = "second";
+          res.headers.set("x-second", "bar");
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [myPlugin(), secondPlugin()],
+    });
+
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.headers["x-first"]).toBe("foo");
+    expect(res.headers["x-second"]).toBe("bar");
+    expect(res.data).toBe("second");
+  });
+});
+
+describe("axios:beforeRequest+afterResponse -", () => {
+  test("declare beforeRequest and afterResponse currently", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const myPlugin = (): Plugin => ({
+      name: "myPlugin",
+      hooks: {
+        beforeRequest: async (req) => {
+          req.headers.set("x-request-plugin", "activate");
+          return req;
+        },
+        afterResponse: async (res) => {
+          console.log("res: ", res);
+          if (res.body === "request plugin is activated") {
+            res.headers.set("x-request-plugin", "succeed");
+            return res;
+          }
+          res.headers.set("x-request-plugin", "failed");
           return res;
         },
       },
@@ -28,15 +211,132 @@ describe("axios -", () => {
 
     axiosInstance.interceptors.request.use(axiosMiddleware.request);
     axiosInstance.interceptors.response.use(axiosMiddleware.response);
-
     const res = await axiosInstance.get("/api/v1/foo");
-    const customHeader = (res.headers as AxiosHeaders)["x-foo"];
-    // const customHeader = res.headers.get("x-foo");
 
-    if (!customHeader) {
-      throw new Error("custom header not found");
-    }
+    expect(res.headers["x-request-plugin"]).toBe("succeed");
+  });
 
-    expect(customHeader).toStrictEqual("bar");
+  test("declare beforeRequest and afterResponse sequentially", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const beforeRequestPlugin = (): Plugin => ({
+      name: "beforeRequestPlugin",
+      hooks: {
+        beforeRequest: async (req) => {
+          req.headers.set("x-request-plugin", "activate");
+          return req;
+        },
+      },
+    });
+    const afterResponsePlugin = (): Plugin => ({
+      name: "afterResponsePlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          if (res.body === "request plugin is activated") {
+            res.headers.set("x-request-plugin", "succeed");
+            return res;
+          }
+          res.headers.set("x-request-plugin", "failed");
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [beforeRequestPlugin(), afterResponsePlugin()],
+    });
+
+    axiosInstance.interceptors.request.use(axiosMiddleware.request);
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.headers["x-request-plugin"]).toBe("succeed");
+  });
+
+  test("declare beforeRequest and afterResponse in reverse order", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const beforeRequestPlugin = (): Plugin => ({
+      name: "beforeRequestPlugin",
+      hooks: {
+        beforeRequest: async (req) => {
+          req.headers.set("x-request-plugin", "activate");
+          return req;
+        },
+      },
+    });
+    const afterResponsePlugin = (): Plugin => ({
+      name: "afterResponsePlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          if (res.body === "request plugin is activated") {
+            res.headers.set("x-request-plugin", "succeed");
+            return res;
+          }
+          res.headers.set("x-request-plugin", "failed");
+          return res;
+        },
+      },
+    });
+
+    const axiosMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [afterResponsePlugin(), beforeRequestPlugin()],
+    });
+
+    axiosInstance.interceptors.request.use(axiosMiddleware.request);
+    axiosInstance.interceptors.response.use(axiosMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.headers["x-request-plugin"]).toBe("succeed");
+  });
+
+  test("use requestMiddleware and responseMiddleware individually", async () => {
+    const axiosInstance = axios.create({
+      baseURL: "https://example.com",
+    });
+
+    const beforeRequestPlugin = (): Plugin => ({
+      name: "beforeRequestPlugin",
+      hooks: {
+        beforeRequest: async (req) => {
+          req.headers.set("x-request-plugin", "activate");
+          return req;
+        },
+      },
+    });
+    const afterResponsePlugin = (): Plugin => ({
+      name: "afterResponsePlugin",
+      hooks: {
+        afterResponse: async (res) => {
+          if (res.body === "request plugin is activated") {
+            res.headers.set("x-request-plugin", "succeed");
+            return res;
+          }
+          res.headers.set("x-request-plugin", "failed");
+          return res;
+        },
+      },
+    });
+
+    const requestMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [beforeRequestPlugin()],
+    });
+    const responseMiddleware = createAxiosInterceptors({
+      client: axiosInstance,
+      plugins: [afterResponsePlugin()],
+    });
+
+    axiosInstance.interceptors.request.use(requestMiddleware.request);
+    axiosInstance.interceptors.response.use(responseMiddleware.response);
+    const res = await axiosInstance.get("/api/v1/foo");
+
+    expect(res.headers["x-request-plugin"]).toBe("succeed");
   });
 });
