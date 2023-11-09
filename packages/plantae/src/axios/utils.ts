40c4d691bd9659e29c9186b8e504bfa0de8544b1
JH.Lee <contact@jins.dev>
2023-11-09 13:49:42 +0900

    chore: revert dynamic import
    This reverts commit 8f74eb695b989c5a2956f098790238ca1b3ca32c.

--- packages/plantae/src/axios/utils.ts
+++ packages/plantae/src/axios/utils.ts
@@ -1,4 +1,5 @@
 import type { AxiosResponse } from "axios";
+import { AxiosError } from "axios";
 
 // https://github.com/axios/axios/blob/7d45ab2e2ad6e59f5475e39afd4b286b1f393fc0/lib/core/settle.js#L15
 export default function settle(
@@ -10,17 +11,15 @@ export default function settle(
   if (!response.status || !validateStatus || validateStatus(response.status)) {
     resolve(response);
   } else {
-    import("axios").then(({ AxiosError }) =>
-      reject(
-        new AxiosError(
-          "Request failed with status code " + response.status,
-          [AxiosError.ERR_BAD_REQUEST, AxiosError.ERR_BAD_RESPONSE][
-            Math.floor(response.status / 100) - 4
-          ],
-          response.config,
-          response.request,
-          response
-        )
+    reject(
+      new AxiosError(
+        "Request failed with status code " + response.status,
+        [AxiosError.ERR_BAD_REQUEST, AxiosError.ERR_BAD_RESPONSE][
+          Math.floor(response.status / 100) - 4
+        ],
+        response.config,
+        response.request,
+        response
       )
     );
   }
