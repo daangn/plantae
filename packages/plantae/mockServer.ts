8ffc37c22004fe8e3d410afe9c01786b9cdd32b6
JH.Lee <contact@jins.dev>
2023-11-14 19:10:31 +0900

    fix: migrate to msw v2

--- packages/plantae/mockServer.ts
+++ packages/plantae/mockServer.ts
@@ -1,42 +1,42 @@
-import { rest } from "msw";
+import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
 
 export const BASE_URL = "https://example.com";
 
 const handlers = [
-  rest.get("https://example.com/api/v1/foo", (req, res, ctx) => {
-    if (req.headers.get("x-request-plugin") === "activate") {
-      return res(ctx.text("request plugin is activated"));
+  http.get("https://example.com/api/v1/foo", ({ request }) => {
+    if (request.headers.get("x-request-plugin") === "activate") {
+      return HttpResponse.text("request plugin is activated");
     }
 
     const mockJson = {
       foo: "bar",
     };
-    return res(ctx.json(mockJson));
+    return HttpResponse.json(mockJson);
   }),
-  rest.post("https://example.com/api/v1/foo", async (req, res, ctx) => {
-    return res(ctx.text("post request is completed"));
+  http.post("https://example.com/api/v1/foo", () => {
+    return HttpResponse.text("post request is completed");
   }),
-  rest.post("https://example.com/api/v1/bar", async (req, res, ctx) => {
-    const body = await req.json();
-    return res(ctx.json(body));
+  http.post("https://example.com/api/v1/bar", async ({ request }) => {
+    const body = await request.json();
+    return HttpResponse.json(body);
   }),
-  rest.get("https://example.com/api/v1/delayed", async (req, res, ctx) => {
+  http.get("https://example.com/api/v1/delayed", async () => {
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    return res(ctx.text("delayed"));
+    return HttpResponse.text("delayed");
   }),
-  rest.get("https://example-second.com/api/v1/foo", async (req, res, ctx) => {
-    return res(ctx.text("url is modified"));
+  http.get("https://example-second.com/api/v1/foo", () => {
+    return HttpResponse.text("url is modified");
   }),
 
-  rest.all(`${BASE_URL}/header/:header`, async (req, res, ctx) => {
-    const header = req.params.header;
+  http.all(`${BASE_URL}/header/:header`, ({ request, params }) => {
+    const header = params.header;
 
-    const values = [...req.headers.entries()]
+    const values = [...request.headers.entries()]
       .filter(([key]) => key === header)
       .map(([, value]) => value);
 
-    return res(ctx.json(values));
+    return HttpResponse.json(values);
   }),
 ];
 
